8fdb05e30d12e0388737201df236200e8d8c23ba
Paul Pan <panjunhong05@gmail.com>
2020-10-15 15:43:44 -0700

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript/base',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 6,
+    project: './tsconfig.json',
+    sourceType: 'module',
+  },
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    'no-await-in-loop': 0,
+  },
+};
---
+++ index.ts
@@ -0,0 +1,7 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+
+const handler = (event) => {
+}
+
+export { handler as default };
+
