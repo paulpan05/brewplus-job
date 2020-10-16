63d2da8c02a934ef1698c84f3ad31df69b8d80da
Paul Pan <panjunhong05@gmail.com>
2020-10-15 19:05:59 -0700

    Milestone commit.

--- index.ts
+++ index.ts
@@ -1,7 +1,55 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
+import { APIGatewayProxyEvent, Context } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+import fetch, { Response } from 'node-fetch';
 
-const handler = (event) => {
-}
+const handler = async (event: APIGatewayProxyEvent, context: Context) => {
+  const dynamodb = new AWS.DynamoDB.DocumentClient({
+    region: 'us-east-1',
+    credentials: {
+      accessKeyId: 'test',
+      secretAccessKey: 'test',
+    },
+    endpoint: 'http://localhost:8000',
+  });
+  try {
+    let res: Response = await fetch('https://formulae.brew.sh/api/formula.json');
+    if (!res.ok) {
+      return new Response(`${res.statusText}`, { status: res.status });
+    }
+    const items: [{ PutRequest: { Item: AWS.DynamoDB.AttributeMap } }] = (await res.json()).map(
+      (item: { [key: string]: any }) => {
+        const result = AWS.DynamoDB.Converter.input(item);
+        const name = item.name;
+        const full_name = item.full_name;
+        return {
+          PutRequest: {
+            Item: {
+              name,
+              full_name,
+              result,
+            },
+          },
+        };
+      },
+    );
+    for (let i = 0; i < items.length; i += 25) {
+      const params = {
+        RequestItems: {
+          BrewPlusFormulae: items.slice(i, i + 25),
+        },
+      };
+      await dynamodb.batchWrite(params).promise();
+    }
+    return {
+      statusCode: 200,
+      body: 'Success',
+    };
+  } catch (err) {
+    return {
+      statusCode: err.statusCode,
+      body: err.code,
+    };
+  }
+};
 
 export { handler as default };
-
